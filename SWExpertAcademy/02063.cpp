99438c273b62505d50ebc6b16124bcaf354fcc89
Juwan <sirius_75@naver.com>
2022-01-07 17:33:56 +0900

    update 23.01.07

---
+++ SWExpertAcademy/02063.cpp
@@ -0,0 +1,20 @@
+﻿#include <iostream>
+#include <algorithm>
+using namespace std;
+// [SWExpertAcademy] 2063. 중간값 찾기
+// 2023.01.06
+
+int main() {
+	int arr[201] = { 0 };
+	int n;
+
+	cin >> n;
+	for (int i = 0; i < n; i++) {
+		cin >> arr[i];
+	}
+	sort(arr, arr + n);
+
+	cout << arr[n / 2];
+
+	return 0;
+}
