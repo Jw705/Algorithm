27a9d6c2908716904bc9848da3b2422ddd681a11
Juwan <sirius_75@naver.com>
2023-01-13 08:28:15 +0900

    rename

---
+++ SWExpertAcademy/p1-3.cpp
@@ -0,0 +1,69 @@
+﻿#include <stdio.h>
+
+typedef struct locate {
+  int x;
+  int y;
+} loc;
+
+int find_path(char map[][30], int visit[30], loc n, int cnt, int r, int c) {
+
+  int max_rst = cnt;
+  loc new_loc;
+  int a[4] = {-1, 0, 1, 0};
+  int b[4] = {0, 1, 0, -1};
+
+  for (int i = 0; i < 4; i++) {
+
+    new_loc.x = n.x + a[i];
+    new_loc.y = n.y + b[i];
+    if (new_loc.x >= r || new_loc.x < 0 || new_loc.y >= c || new_loc.y < 0)
+      continue;
+
+    int charr = map[new_loc.x][new_loc.y] - 'A';
+    if (visit[charr] == 0) {
+
+      visit[charr]++;
+      int new_rst;
+      new_rst = find_path(map, visit, new_loc, cnt + 1, r, c);
+      if (new_rst == 26) {
+        return 26;
+      }
+      max_rst = (max_rst < new_rst) ? new_rst : max_rst;
+
+      visit[charr]--;
+    }
+  }
+  return max_rst;
+}
+
+int main() {
+
+  int T;
+  scanf("%d", &T);
+  for (int test_case = 0; test_case < T; test_case++) {
+    int r, c;
+    char map[30][30] = {
+        0,
+    };
+    int visit[30] = {
+        0,
+    };
+    scanf("%d %d", &r, &c);
+    for (int i = 0; i < r; i++) {
+      for (int j = 0; j < c; j++) {
+        scanf(" %c", &map[i][j]);
+      }
+    }
+
+    loc n;
+    n.x = 0;
+    n.y = 0;
+
+    visit[map[0][0] - 'A']++;
+
+    int rst;
+    rst = find_path(map, visit, n, 1, r, c);
+    printf("#%d %d\n", test_case + 1, rst);
+  }
+  return 0;
+}
---
+++ SWExpertAcademy/p2-1.cpp
@@ -0,0 +1,74 @@
+﻿#include <iostream>
+#include <vector>
+#include <algorithm>
+#include <unordered_map>
+
+using namespace std;
+
+const vector<vector<int>> drc = {
+    // wasd
+    {-1, 0}, {0, -1}, {1, 0}, {0, 1}
+};
+
+void Init();
+int Dfs(const int r, const int c, const int visited);
+
+int R, C;
+vector<vector<int>> board;
+vector<vector<unordered_map<int, int>>> dp;
+
+int main() {
+    cin.tie(nullptr)->sync_with_stdio(false);
+    
+    int T;
+    cin >> T;
+    
+    for(int t = 1; t <= T; t++) {
+        Init();
+        cout << '#' << t << ' ' << Dfs(0, 0, 0) << ' ' << '\n';
+    }
+    return 0;
+}
+
+void Init() {
+    cin >> R >> C;
+    
+    board.resize(R);
+    for (int r = 0; r < R; r++) {
+        board[r].resize(C);
+        for (int c = 0; c < C; c++) {
+            char ch;
+            cin >> ws >> ch;
+            board[r][c] = ch - 'A';
+        }
+    }
+    
+    dp.resize(R);
+    for (int r = 0; r < R; r++) {
+        dp[r].resize(C);
+        for (int c = 0; c < C; c++) {
+            dp[r][c] = {};
+        }
+    }
+}
+
+int Dfs(const int r, const int c, const int visited) {
+    const int bit = 1 << board[r][c];
+    if (visited & bit) {
+        return 0;
+    }
+    if (dp[r][c].find(visited) != dp[r][c].end()) {
+        return dp[r][c][visited];
+    }
+    int max_answer = 0;
+    for (int d = 0; d < 4; d++) {
+        const int nr = r + drc[d][0],
+                nc = c + drc[d][1];
+        if (nr >= 0 && nr < R
+                && nc >= 0 && nc < C) {
+            max_answer = max(max_answer, Dfs(nr, nc, visited | bit));
+        }
+    }
+    dp[r][c][visited] = ++max_answer;
+    return max_answer;
+}
