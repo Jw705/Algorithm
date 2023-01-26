1662123a30f349e74deae5b46145481c2521b9f9
Juwan <sirius_75@naver.com>
2023-01-26 21:59:37 +0900

    update 23.01.26

---
+++ Baekjoon/10870 피보나치.cpp
@@ -0,0 +1,23 @@
+﻿#include <iostream>
+using namespace std;
+// [baekjoon] 10870번 - 피보나치 수 5
+// 2023.01.26
+
+int fibonacci(int n) {
+	if (n <= 1) {
+		return n;
+	}
+	else {
+		return fibonacci(n - 1) + fibonacci(n - 2);
+	}
+}
+
+int main(void) {
+
+	int n;
+	cin >> n;
+
+	cout << fibonacci(n);
+
+	return 0;
+}
