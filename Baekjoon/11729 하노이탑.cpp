699cea59361188fba9a62ae1d8be6027db0d53d7
Juwan <sirius_75@naver.com>
2023-01-31 16:42:39 +0900

    update 23.01.31

---
+++ Baekjoon/11729 하노이탑.cpp
@@ -0,0 +1,33 @@
+﻿#include <iostream>
+#include <string>
+#include <cmath>
+using namespace std;
+// [baekjoon] 11729번 - 하노이 탑 이동 순서
+// 2023.01.31
+
+string str;
+
+void hanoi(int n, char from, char aux, char to) {
+	if (n == 1) {
+		cout << from << " " << to << "\n";
+		return;
+	}
+	hanoi(n - 1, from, to, aux);
+	cout << from << " " << to << "\n";
+	hanoi(n - 1, aux, from, to);
+}
+
+int main() {
+
+	cin.tie(NULL);
+	ios::sync_with_stdio(false);
+
+	int n;
+	cin >> n;
+	cout << (int)pow(2, n) - 1 << '\n';
+
+	hanoi(n, '1', '2', '3');
+
+	return 0;
+}
+
