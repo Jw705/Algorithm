b8cf320d94cd57cea43bc728a60f1288650205c9
Juwan <sirius_75@naver.com>
2023-02-01 19:16:28 +0900

    update 23.02.01

---
+++ Baekjoon/07568.cpp
@@ -0,0 +1,38 @@
+﻿#include <iostream>
+#include <vector>
+using namespace std;
+// [baekjoon] 07568번 - 덩치
+// 2023.01.31
+
+int main() {
+
+	cin.tie(NULL);
+	ios::sync_with_stdio(false);
+
+	vector<int> v1;
+	vector<int> v2;
+
+
+	int n;
+	cin >> n;
+
+	for (int i = 0; i < n; i++) {
+		int x, y;
+		cin >> x >> y;
+		v1.push_back(x);
+		v2.push_back(y);
+	}
+
+	for (int i = 0; i < n; i++) {
+		int rank = 1;
+		for (int j = 0; j < n; j++) {
+			if (i != j && v1[i] < v1[j] && v2[i] < v2[j]) {
+				rank++;
+			}
+		}
+		cout << rank << " ";
+	}
+
+	return 0;
+}
+
---
+++ Baekjoon/10816 이진탐색 upper_bound 사용.cpp
@@ -0,0 +1,38 @@
+﻿#include <iostream>
+#include <vector>
+#include <algorithm>
+using namespace std;
+// [baekjoon] 10816번 - 숫자 카드 2
+// 2023.01.31
+
+
+int main() {
+
+	cin.tie(NULL);
+	ios::sync_with_stdio(false);
+
+	vector<int> v1;
+	int n, m, tmp;
+	cin >> n;
+
+	for (int i = 0; i < n; i++) {
+		cin >> tmp;
+		v1.push_back(tmp);
+	}
+
+	sort(v1.begin(), v1.end());
+
+	cin >> m;
+
+	for (int i = 0; i < m; i++) {
+		cin >> tmp;
+
+		int upper = upper_bound(v1.begin(), v1.end(), tmp) - v1.begin();
+		int lower = lower_bound(v1.begin(), v1.end(), tmp) - v1.begin();
+
+		cout << upper - lower << " ";
+	}
+
+	return 0;
+}
+
