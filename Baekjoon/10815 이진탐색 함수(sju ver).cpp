d16fdd6a1fe0886f034d0d4183b69421ba304567
Juwan <sirius_75@naver.com>
2023-02-02 16:20:23 +0900

    update 23.02.02

---
+++ Baekjoon/10815 이진탐색 함수(sju ver).cpp
@@ -0,0 +1,58 @@
+﻿#include <iostream>
+#include <vector>
+#include <algorithm>
+using namespace std;
+// [baekjoon] 10815번 - 숫자 카드
+// 2023.01.31
+
+void rFindElement(vector<int>& L, int k, int start, int end) {
+
+	if (start > end) {		// base case
+		cout << "0 ";
+		return;				// k가 없는 경우
+	}
+
+	int mid = (start + end) / 2;
+
+	if (L[mid] == k) {
+		cout << "1 ";		// k를 찾은 경우
+		return;
+	}
+	else if (L[mid] > k) {
+		return rFindElement(L, k, start, mid - 1);
+	}
+	else {
+		return rFindElement(L, k, mid + 1, end);
+	}
+}
+
+void findElement(vector<int>& L, int k, int n) {
+	return rFindElement(L, k, 0, n - 1);
+}
+
+int main() {
+
+	cin.tie(NULL);
+	ios::sync_with_stdio(false);
+
+	vector<int> v1;
+	int n, m, tmp;
+	cin >> n;
+
+	for (int i = 0; i < n; i++) {
+		cin >> tmp;
+		v1.push_back(tmp);
+	}
+
+	sort(v1.begin(), v1.end());
+
+	cin >> m;
+
+	for (int i = 0; i < m; i++) {
+		cin >> tmp; 
+		findElement(v1, tmp, n);
+	}
+
+	return 0;
+}
+
--- Baekjoon/14425 문자열 이진탐색.cpp
+++ Baekjoon/14425 문자열 이진탐색.cpp
@@ -1,32 +1,35 @@
 ﻿#include <iostream>
 #include <vector>
+#include <string>
 #include <algorithm>
 using namespace std;
-// [baekjoon] 10815번 - 숫자 카드
-// 2023.01.31
+// [baekjoon] 14425번 - 문자열 집합
+// 2023.02.01
 
 int main() {
 
 	cin.tie(NULL);
 	ios::sync_with_stdio(false);
 
-	vector<int> v1;
-	int n, m, tmp;
-	cin >> n;
+	vector<string> v;
+	int n, m;
+	string tmp;
+	cin >> n >> m;
 
 	for (int i = 0; i < n; i++) {
 		cin >> tmp;
-		v1.push_back(tmp);
+		v.push_back(tmp);
 	}
 
-	sort(v1.begin(), v1.end());
+	sort(v.begin(), v.end());
 
-	cin >> m;
-
+	int cnt = 0;
 	for (int i = 0; i < m; i++) {
 		cin >> tmp;
-		cout << binary_search(v1.begin(), v1.end(), tmp) << " ";
+		cnt += binary_search(v.begin(), v.end(), tmp);
 	}
+
+	cout << cnt;
 
 	return 0;
 }
