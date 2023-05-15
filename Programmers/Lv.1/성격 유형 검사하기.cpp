5fc6e7e1eaf66e1823572511503c03331f6c102c
Juwan <sirius_75@naver.com>
2023-05-15 19:16:13 +0900

    프로그래머스 업데이트

--- Programmers/Lv.1/성격 유형 검사하기.cpp
+++ Programmers/Lv.1/성격 유형 검사하기.cpp
@@ -5,51 +5,51 @@ using namespace std;
 
 string solution(vector<string> survey, vector<int> choices) {
     string answer = "";
-    char list[8]={'R','T','C','F','J','M','A','N'};
-    int RTCFJMAN[8]={0};
+    char typeName[8]={'R','T','C','F','J','M','A','N'};
+    int typeScore[8]={0};
     for(int i=0;i<survey.size();i++){
         if (choices[i]==1) {            
             for(int j=0;j<8;j++){
-                if(list[j]==survey[i][0])
-                    RTCFJMAN[j]+=3;
+                if(typeName[j]==survey[i][0])
+                    typeScore[j]+=3;
             }
         }
         else if (choices[i]==2) {            
             for(int j=0;j<8;j++){
-                if(list[j]==survey[i][0])
-                    RTCFJMAN[j]+=2;
+                if(typeName[j]==survey[i][0])
+                    typeScore[j]+=2;
             }
         }
         else if (choices[i]==3) {            
             for(int j=0;j<8;j++){
-                if(list[j]==survey[i][0])
-                    RTCFJMAN[j]+=1;
+                if(typeName[j]==survey[i][0])
+                    typeScore[j]+=1;
             }
         }
 
         else if (choices[i]==5) {            
             for(int j=0;j<8;j++){
-                if(list[j]==survey[i][1])
-                    RTCFJMAN[j]+=1;
+                if(typeName[j]==survey[i][1])
+                    typeScore[j]+=1;
             }
         }        
         else if (choices[i]==6) {            
             for(int j=0;j<8;j++){
-                if(list[j]==survey[i][1])
-                    RTCFJMAN[j]+=2;
+                if(typeName[j]==survey[i][1])
+                    typeScore[j]+=2;
             }
         }        
         else if (choices[i]==7) {            
             for(int j=0;j<8;j++){
-                if(list[j]==survey[i][1])
-                    RTCFJMAN[j]+=3;
+                if(typeName[j]==survey[i][1])
+                    typeScore[j]+=3;
             }
         }  
     }
 
-    answer += RTCFJMAN[0] >= RTCFJMAN[1] ? 'R' : 'T';
-    answer += RTCFJMAN[2] >= RTCFJMAN[3] ? 'C' : 'F';
-    answer += RTCFJMAN[4] >= RTCFJMAN[5] ? 'J' : 'M';
-    answer += RTCFJMAN[6] >= RTCFJMAN[7] ? 'A' : 'N';
+    answer += typeScore[0] >= typeScore[1] ? 'R' : 'T';
+    answer += typeScore[2] >= typeScore[3] ? 'C' : 'F';
+    answer += typeScore[4] >= typeScore[5] ? 'J' : 'M';
+    answer += typeScore[6] >= typeScore[7] ? 'A' : 'N';
     return answer;
 }
