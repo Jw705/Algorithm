cbbf789c448728a9c5d35db71ae803ea5322cb14
Juwan <sirius_75@naver.com>
2023-05-23 22:15:04 +0900

    update typo

--- Programmers/Lv.1/달리기 경주 (map2).cpp
+++ Programmers/Lv.1/달리기 경주 (map2).cpp
@@ -4,29 +4,26 @@
 
 using namespace std;
 
-vector<string> solution(vector<string> players, vector<string> callings) {
-    vector<string> answer;
-    map<int,string> m_name; // 순위가 key, 이름이 value
-    map<string,int> m_rank; // 이름이 key, 순위가 value
-    for(int i=0;i<players.size();i++)
-    {
-        m_name[i] = players[i];
-        m_rank[players[i]] = i;
+vector<string> solution(vector<string> players, vector<string> callings) {  
+      
+    vector<string> answer(players);
+    
+    map<string, int> m1;            // 플레이어의 이름과 순위를 저장할 map
+    
+    for (int i = 0; i < players.size(); i++) {
+        m1[players[i]] = i; // 플레이어의 이름과 순위 저장
     }
     
     for(int i=0;i<callings.size();i++){
-        int locate = m_rank[callings[i]];           // 현재 해설이 부른 선수의 순위
-        string prevPlayerName = m_name[locate-1];   // 그 선수 앞에 있는 선수의 이름
-            
-        m_name[locate-1] = callings[i];
-        m_name[locate] = prevPlayerName;
-        m_rank[callings[i]] = locate-1;
-        m_rank[prevPlayerName] = locate;
+        // map을 이용하여 호출된 선수의 등수 O(logn)시간에 탐색
+        int locate = m1[callings[i]];
+        // map에 저장된 순위 갱신
+        m1[answer[locate-1]] +=1;
+        m1[callings[i]] -=1;
+        // answer에 저장된 순서 변경
+        string tmp = answer[locate];
+        answer[locate] = answer[locate-1];
+        answer[locate-1] = tmp;   
     }
-    
-    // m_name의 모든 value를 answer에 넣어준다.
-    for(auto c : m_name) 
-        answer.push_back(c.second);
-    
     return answer;
 }
--- Programmers/Lv.1/달리기 경주 (시간초과).cpp
+++ Programmers/Lv.1/달리기 경주 (시간초과).cpp
@@ -1,21 +1,19 @@
 #include <string>
 #include <vector>
-#include <iostream>
 #include <algorithm>
 
 using namespace std;
 
 vector<string> solution(vector<string> players, vector<string> callings) {
     
+    vector<string> answer(players);
+    
     for(int i=0;i<callings.size();i++){
-        int locate = find(players.begin(), players.end(), callings[i]) - players.begin();
-        string tmp = players[locate];
-        players[locate] = players[locate-1];
-        players[locate-1] = tmp;
+        int locate = find(answer.begin(), answer.end(), callings[i]) - answer.begin();
+        string tmp = answer[locate];
+        answer[locate] = answer[locate-1];
+        answer[locate-1] = tmp;
     }
     
-    vector<string> answer(players);
     return answer;
 }
-
-// 시간복잡도 줄이기
