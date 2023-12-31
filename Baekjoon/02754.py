f085249415a1fd2749cbaeb876772780daa16981
Juwan <sirius_75@naver.com>
2023-12-31 21:06:57 +0900

    feat: update 23.12.31
    23.12.31 풀이 내용입니다.

---
+++ Baekjoon/02754.py
@@ -0,0 +1,28 @@
+GRADE = input()
+
+if GRADE == 'A+':
+    print(4.3)
+elif GRADE == 'A0':
+    print(4.0)
+elif GRADE == 'A-':
+    print(3.7)
+elif GRADE == 'B+':
+    print(3.3)
+elif GRADE == 'B0':
+    print(3.0)
+elif GRADE == 'B-':
+    print(2.7)
+elif GRADE == 'C+':
+    print(2.3)
+elif GRADE == 'C0':
+    print(2.0)
+elif GRADE == 'C-':
+    print(1.7)
+elif GRADE == 'D+':
+    print(1.3)
+elif GRADE == 'D0':
+    print(1.0)
+elif GRADE == 'D-':
+    print(0.7)
+elif GRADE == 'F':
+    print(0.0)
