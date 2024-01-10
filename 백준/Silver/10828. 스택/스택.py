293f052205f1287e7d6e503ec7855dafdeb3fa94
Juwan Jeong <86391351+Jw705@users.noreply.github.com>
2024-01-10 10:47:53 +0900

    [Silver IV] Title: 스택, Time: 80 ms, Memory: 34052 KB -BaekjoonHub

---
+++ 백준/Silver/10828. 스택/스택.py
@@ -0,0 +1,29 @@
+import sys
+from collections import deque
+
+n = int(sys.stdin.readline())
+
+queue = deque()
+
+for _ in range(n):
+    data = list(sys.stdin.readline().split())
+
+    if data[0] == 'push':
+        queue.append(int(data[1]))
+    elif data[0] == 'pop':
+        if len(queue) == 0:
+            print(-1)
+        else:
+            print(queue.pop())
+    elif data[0] == 'size':
+        print(len(queue))
+    elif data[0] == 'empty':
+        if len(queue) == 0:
+            print(1)
+        else:
+            print(0)
+    elif data[0] == 'top':
+        if len(queue) == 0:
+            print(-1)
+        else:
+            print(queue[len(queue) - 1])
