7294a3c6419436fb1d4fac09c97f86cc4ed0f33a
Juwan Jeong <86391351+Jw705@users.noreply.github.com>
2024-01-28 23:28:04 +0900

    [Gold II] Title: 골목 대장 호석 - 효율성 2, Time: 9848 ms, Memory: 194292 KB, Score: 42 point -BaekjoonHub

--- 백준/Gold/20183. 골목 대장 호석 － 효율성 2/골목 대장 호석 － 효율성 2.py
+++ 백준/Gold/20183. 골목 대장 호석 － 효율성 2/골목 대장 호석 － 효율성 2.py
@@ -8,31 +8,59 @@ n, m, a, b, c = map(int, input().split())
 graph = [[] for _ in range(n + 1)]
 # 노드의 방문 여부를 저장할 리스트 초기화
 visited = [False] * (n + 1)
+costs = []
 
 # 입력을 받아 트리 생성
 for i in range(0, m):
     data = list(map(int, sys.stdin.readline().split()))
     graph[data[0]].append([data[1], data[2]])
     graph[data[1]].append([data[0], data[2]])
+    costs.append(data[2])
 
-result = 10**10
+result = 10 ** 11
+tmp = False
 
 
 def dfs(v, money, max_charge):
-    global result
+    global tmp
     if v == b:
         # print([v, money, max_charge], end=' ')
-        result = min(result, max_charge)
+        tmp = True
+        return True
 
     else:
         visited[v] = True
         # print(v, end=' ')
         for i in graph[v]:
-            if (not visited[i[0]]) and money - i[1] >= 0:
-                dfs(i[0], money - i[1], max(max_charge, i[1]))
+            if (not visited[i[0]]) and money - i[1] >= 0 and max_charge >= i[1]:
+                dfs(i[0], money - i[1], max_charge)
 
 
-dfs(a, c, 0)
-if result == 10**10:
+'''
+for i in range(len(costs)):
+    tmp = False
+    visited = [False] * (n + 1)
+    dfs(a, c, costs[i])
+    # print(costs[i], tmp)
+    if tmp:
+        result = min(result, costs[i])
+'''
+
+costs.sort()
+left, right = 0, len(costs) - 1
+while left <= right:
+    mid = (left + right) // 2
+
+    tmp = False
+    visited = [False] * (n + 1)
+    dfs(a, c, costs[mid])
+    # print(costs[i], tmp)
+    if tmp:
+        right = mid - 1
+        result = min(result, costs[mid])
+    else:
+        left = mid + 1
+
+if result == 10 ** 11:
     result = -1
 print(result)
