c9b40c6a82fed7e7f67080b58dd9d1e9753f5491
Juwan Jeong <86391351+Jw705@users.noreply.github.com>
2024-02-14 20:31:11 +0900

    [Gold V] Title: 평범한 배낭, Time: 5184 ms, Memory: 227036 KB -BaekjoonHub

---
+++ 백준/Gold V/12865. 평범한 배낭/평범한 배낭.py
@@ -0,0 +1,23 @@
+import sys
+
+input = sys.stdin.readline
+n, k = map(int, input().split())
+
+dp = [[0] * (k + 1) for _ in range(n + 1)]
+# dp[가방 최대 무게][물건의 개수]
+arr = [[0,0]]
+
+for _ in range(n):
+    weight, value = map(int, input().split())
+    arr.append([weight, value])
+
+for i in range(1, n + 1):
+    for j in range(1, k + 1):
+        w, v = arr[i]
+        if j >= w:
+            dp[i][j] = max(dp[i - 1][j], dp[i - 1][j - w] + v)
+        else:
+            dp[i][j] = dp[i - 1][j]
+
+
+print(dp[n][k])
