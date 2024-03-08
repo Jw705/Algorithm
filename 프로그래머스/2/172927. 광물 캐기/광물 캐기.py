9305730edb7e4c9c545573a0a1c50784a73325de
Juwan Jeong <86391351+Jw705@users.noreply.github.com>
2024-03-08 10:04:23 +0900

    [level 2] Title: 광물 캐기, Time: 0.04 ms, Memory: 10.4 MB -BaekjoonHub

---
+++ 프로그래머스/2/172927. 광물 캐기/광물 캐기.py
@@ -0,0 +1,37 @@
+def solution(picks, minerals):
+    answer = 0
+    피로도 = [[1,1,1],[5,1,1],[25,5,1]]
+    
+    costs=[]
+    for i in range(0,sum(picks)*5,5):
+        cost = [0,0,0]
+        for j in range(i,i+5):
+            if j<len(minerals):
+                if (minerals[j] == 'diamond'):
+                    cost[0]+=피로도[0][0]
+                    cost[1]+=피로도[1][0]
+                    cost[2]+=피로도[2][0]
+                elif (minerals[j] == 'iron'):
+                    cost[0]+=피로도[0][1]
+                    cost[1]+=피로도[1][1]
+                    cost[2]+=피로도[2][1]
+                elif (minerals[j] == 'stone'):
+                    cost[0]+=피로도[0][2]
+                    cost[1]+=피로도[1][2]
+                    cost[2]+=피로도[2][2]
+        costs.append(cost)
+        
+    costs.sort(key = lambda x : x[2], reverse=True)
+        
+    for cost in costs:
+        if picks[0] > 0:
+            answer+=cost[0]
+            picks[0]-=1
+        elif picks[1] > 0:
+            answer+=cost[1]
+            picks[1]-=1
+        elif picks[2] > 0:
+            answer+=cost[2]
+            picks[2]-=1            
+    
+    return answer
