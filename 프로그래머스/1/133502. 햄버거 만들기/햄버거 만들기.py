2db90915ab376ef6acf4e5daa3a3c91023d53c49
Juwan Jeong <86391351+Jw705@users.noreply.github.com>
2024-03-10 20:34:55 +0900

    [level 1] Title: 햄버거 만들기, Time: 0.01 ms, Memory: 10.3 MB -BaekjoonHub

--- 프로그래머스/1/133502. 햄버거 만들기/햄버거 만들기.py
+++ 프로그래머스/1/133502. 햄버거 만들기/햄버거 만들기.py
@@ -3,12 +3,10 @@ def solution(ingredient):
     stack=[]
     for i in ingredient:
         stack.append(i)
-        if i==1 and len(stack) >= 4:
-            if stack[-1] == 1 and stack[-2] == 3 and stack[-3] == 2 and stack[-4] == 1:
-                stack.pop()
-                stack.pop()
-                stack.pop()
-                stack.pop()
-                answer+=1
-            
+        if stack[-4:] == [1,2,3,1]:
+            stack.pop()
+            stack.pop()
+            stack.pop()
+            stack.pop()
+            answer+=1            
     return answer
