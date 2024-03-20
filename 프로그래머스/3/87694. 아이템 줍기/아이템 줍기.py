d94295ef936f179552ecfd0349e621a8288ed825
Juwan Jeong <86391351+Jw705@users.noreply.github.com>
2024-03-20 21:43:14 +0900

    [level 3] Title: 아이템 줍기, Time: 3.09 ms, Memory: 10.5 MB -BaekjoonHub

--- 프로그래머스/3/87694. 아이템 줍기/아이템 줍기.py
+++ 프로그래머스/3/87694. 아이템 줍기/아이템 줍기.py
@@ -1,38 +1,36 @@
 from collections import deque
 
-def has_zero_around(arr, y, x):
+def has_zero_around(arr, x, y):
     for dy in range(-1, 2):
         for dx in range(-1, 2):
-            if arr[y + dy][x + dx] == 0:
+            if arr[x + dx][y + dy] == 0:
                 return True
     return False
 
-def bfs(graph,start,target,visited):    
+def bfs(graph,start,target):    
     sx,sy = start
     queue = deque([(sx,sy,0)])    
-    visited[sx][sy]=1
+    graph[sx][sy]=0
     
     directions=[[1,0],[-1,0],[0,-1],[0,1]]    
     while queue:
-        x, y, l = queue.popleft()
+        x, y, l = queue.popleft()        
+        
         if [x,y] == target:
             return l/2
+        
         for direction in directions:
             dx, dy = direction
             nx = x+dx
             ny = y+dy
             
-            조건1 = 0<nx<101 and 0<ny<101
-            조건2 = (visited[nx][ny] == 0)
-            조건3 = (graph[nx][ny] == 1)
-            if 조건1 and 조건2 and 조건3:   
+            if graph[nx][ny] == 1:   
                 queue.append([nx,ny,l+1])
-                visited[nx][ny] = 1
+                graph[nx][ny]=0
 
 def solution(rectangle, characterX, characterY, itemX, itemY):
     fill=[[0 for _ in range(105)]for _ in range(105)]
     graph=[[0 for _ in range(105)]for _ in range(105)]
-    visited=[[0 for _ in range(105)]for _ in range(105)]
     
     for rect in rectangle:
         sx,sy,ex,ey=rect
@@ -45,6 +43,6 @@ def solution(rectangle, characterX, characterY, itemX, itemY):
             if fill[x][y] == 1 and has_zero_around(fill, x, y):
                 graph[x][y]=1
                 
-    answer = bfs(graph,[characterX*2, characterY*2], [itemX*2, itemY*2],visited)
+    answer = bfs(graph,[characterX*2, characterY*2], [itemX*2, itemY*2])
     
     return answer
