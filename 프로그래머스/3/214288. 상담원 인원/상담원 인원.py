0685335ed6b918ef2e4bf4bb7603211510b3aad2
Juwan Jeong <86391351+Jw705@users.noreply.github.com>
2024-03-21 17:22:10 +0900

    [level 3] Title: 상담원 인원, Time: 669.32 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/3/214288. 상담원 인원/상담원 인원.py
+++ 프로그래머스/3/214288. 상담원 인원/상담원 인원.py
@@ -1,18 +1,14 @@
 from itertools import product
 
-def solution(k, n, reqs):
-          
+def solution(k, n, reqs):          
     arr = [[] for _ in range(k+1)]
     for req in reqs:
         a,b,c=req
         arr[c].append([a,b])
     
-    mans = [1 for _ in range(k+1)]
-    mans[0]=0
+    waiting_time = [[0 for _ in range(n-k+1) ] for _ in range(k+1)]   
     
-    dp = [[0 for _ in range(n-k+1) ] for _ in range(k+1)]   
-    
-    # 멘토의 수 별 대기시간 계산         
+    # 멘토 수 별 대기시간 계산         
     for i in range(1,k+1):
         for j in range(n-k+1):
             time=[0]*(j+1)
@@ -28,34 +24,17 @@ def solution(k, n, reqs):
                         is_resv = True
                         break
                 if is_resv == False:
-                    dp[i][j]+=time[0]-a
+                    waiting_time[i][j]+=time[0]-a
                     time[0]=time[0]+b              
 
     
-    answer = 30000000
+    answer = int(1e9)
+    # 멘토 배치 경우의 수를 모두 찾아 최소 대기시간 갱신
     for p in product(range(1,n+1),repeat=k):
         if sum(p) == n:
             tmp = 0
             for i in range(1,k+1):
-                tmp+=(dp[i][p[i-1]-1])
+                tmp+=(waiting_time[i][p[i-1]-1])
             answer = min(answer,tmp)
 
     return answer
-
-'''
-dp = []
-0 0 0
-
-60
-
-60
-100
-
-5
-3 1 1
-
-
-
-100
-50
-'''
