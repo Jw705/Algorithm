3b42ee99286cd8e6949dfb5c54e229681ab5d035
Juwan Jeong <86391351+Jw705@users.noreply.github.com>
2024-04-29 12:29:25 +0900

    [Gold III] Title: 텀 프로젝트, Time: 3656 ms, Memory: 59140 KB -BaekjoonHub

--- 백준/Gold/9466. 텀 프로젝트/텀 프로젝트.py
+++ 백준/Gold/9466. 텀 프로젝트/텀 프로젝트.py
@@ -4,41 +4,36 @@ input = sys.stdin.readline
 
 t = int(input())
 for i in range(t):
+
     n = int(input())
     data = list(map(int, input().split()))
     data.insert(0, 0)
-    visited = [False] * (n + 1)
 
+    visited = [False] * (n + 1)
     result = n
 
     for j in range(1, len(data)):
         if not visited[j]:
             current_idx = j
-            team_list = {j: 0}
+            # 시작 학생을 임시 팀에 추가, 몇 번째로 추가된 학생인지 저장
+            tmp_team = {j: 0}
             while True:
-                next_idx = data[current_idx]
+                # 현재 학생을 방문처리하고, 현재 학생이 선택한 학생을 다음 학생으로 저장
                 visited[current_idx] = True
+                next_idx = data[current_idx]
 
-                if next_idx in team_list:
-                    result -= (len(team_list) - team_list[next_idx])
+                # 다음 학생이 임시 팀에 들어간 학생이면 (=사이클) 팀이 생성된다.
+                if next_idx in tmp_team:
+                    # 해당 학생부터 시작되는 사이클의 학생만큼만 팀이 되므로, 그 길이를 답에서 뺀다.
+                    result -= (len(tmp_team) - tmp_team[next_idx])
                     break
 
+                # 다음 학생이 이미 탐색한 학생이면, 다시 탐색할 필요가 없으므로 종료
                 if visited[next_idx]:
                     break
 
+                # 다음 학생을 tmp_team에 추가하고 탐색
                 current_idx = next_idx
-                team_list[current_idx] = len(team_list)
+                tmp_team[current_idx] = len(tmp_team)
 
     print(result)
-
-'''
-1
-4
-2 3 4 2
-
-2
-7
-3 1 3 7 3 4 6
-8
-1 2 3 4 5 6 7 8
-'''
