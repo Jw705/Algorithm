bf8e9af91c919817cc5c5f59b9b30e0758cbc1be
Juwan Jeong <86391351+Jw705@users.noreply.github.com>
2024-05-01 15:35:24 +0900

    [Gold III] Title: 할로윈의 양아치, Time: 548 ms, Memory: 119252 KB -BaekjoonHub

--- 백준/Gold/20303. 할로윈의 양아치/할로윈의 양아치.py
+++ 백준/Gold/20303. 할로윈의 양아치/할로윈의 양아치.py
@@ -19,37 +19,32 @@ def union_parent(parent, a, b):
 
 
 n, m, k = map(int, input().split())
-candies = list(map(int, input().split()))
-
+candies = [0] + list(map(int, input().split()))
 parent = [0] * (n + 1)
 for i in range(1, n + 1):
     parent[i] = i
 
-# O(m)
 for i in range(m):
     a, b = map(int, input().split())
     union_parent(parent, a, b)
 
-data = {}
-for i in range(n):
-    tmp = find_parent(parent, i + 1)
-    if tmp not in data:
-        data[tmp] = {'child_num': 1, 'candy_num': candies[i]}
+groups = [[0, 0] for _ in range(n + 1)]
+for i in range(1, n + 1):
+    if i != parent[i]:
+        p = find_parent(parent, i)
+        groups[p][0] = groups[p][0] + 1
+        groups[p][1] = groups[p][1] + candies[i]
     else:
-        data[tmp]['child_num'] = data[tmp]['child_num'] + 1
-        data[tmp]['candy_num'] = data[tmp]['candy_num'] + candies[i]
+        groups[i][0] = 1
+        groups[i][1] = candies[i]
 
-arr = [[0, 0]]
-for d in data.values():
-    arr.append([d['child_num'], d['candy_num']])
+dp = [0 for _ in range(k + 1)]
+for i in range(len(groups)):
+    if groups[i][0] > 0:
+        w, v = groups[i]
+        for j in range(k - 1, w - 1, -1):
+            if j >= w:
+                dp[j] = max(dp[j], dp[j - w] + v)
 
-group_num = len(arr)
-dp = [0 for _ in range(k)]
 
-for i in range(1, group_num):
-    for j in range(k - 1, 0, -1):
-        w, v = arr[i]
-        if j >= w:
-            dp[j] = max(dp[j], dp[j - w] + v)
-            
 print(dp[k - 1])
