73d41bcaacb3df15e28f98a6663623969cc4eed6
Juwan Jeong <86391351+Jw705@users.noreply.github.com>
2024-05-07 09:22:04 +0900

    [Gold II] Title: 꼬인 전깃줄, Time: 224 ms, Memory: 41932 KB -BaekjoonHub

--- 백준/Gold/1365. 꼬인 전깃줄/꼬인 전깃줄.py
+++ 백준/Gold/1365. 꼬인 전깃줄/꼬인 전깃줄.py
@@ -4,7 +4,6 @@ input = sys.stdin.readline
 
 n = int(input())
 data = list(map(int, input().split()))
-dp = [0]
 
 
 # target 보다 작은 값 중 가장 큰 값의 index 반환
@@ -21,12 +20,16 @@ def binary_left(array, target):
     return result
 
 
-for i in range(len(data)):
-    idx = binary_left(dp, data[i]) + 1
-    if idx >= len(dp):
-        dp.append(data[i])
-    else:
-        dp[idx] = min(dp[idx], data[i])
+# 가장 긴 증가하는 부분 수열(LIS)의 길이를 반환하는 함수
+def LIS(array):
+    dp = [0]
+    for i in range(len(data)):
+        idx = binary_left(dp, data[i]) + 1
+        if idx >= len(dp):
+            dp.append(data[i])
+        else:
+            dp[idx] = min(dp[idx], data[i])
+    return len(dp) - 1
 
-LIS = len(dp) - 1
-print(n - LIS)
+
+print(n - LIS(data))
