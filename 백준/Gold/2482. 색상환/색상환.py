5b0e02a23d0e69e2fff5c8fa56cc6ff1ff5be104
Juwan Jeong <86391351+Jw705@users.noreply.github.com>
2024-05-29 17:34:40 +0900

    [Gold III] Title: 색상환, Time: 304 ms, Memory: 58596 KB -BaekjoonHub

---
+++ 백준/Gold/2482. 색상환/색상환.py
@@ -0,0 +1,23 @@
+import sys
+
+input = sys.stdin.readline
+
+n = int(input())
+k = int(input())
+
+dp = [[0 for _ in range(n + 1)] for _ in range(n + 1)]
+
+dp[3][0] = 1
+dp[3][1] = 3
+dp[3][2] = 0
+dp[3][3] = 0
+
+dp[4][0] = 1
+dp[4][1] = 4
+dp[4][2] = 2
+
+for i in range(5, n + 1):
+    for j in range(k + 1):
+        dp[i][j] = dp[i - 2][j - 1] + dp[i - 1][j]
+
+print(dp[n][k] % 1000000003)
