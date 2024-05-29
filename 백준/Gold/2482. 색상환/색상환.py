d6a0eda135f841cc2f0d5a969312dcf395367f5f
Juwan Jeong <86391351+Jw705@users.noreply.github.com>
2024-05-29 17:37:13 +0900

    [Gold III] Title: 색상환, Time: 304 ms, Memory: 58576 KB -BaekjoonHub

--- 백준/Gold/2482. 색상환/색상환.py
+++ 백준/Gold/2482. 색상환/색상환.py
@@ -7,16 +7,13 @@ k = int(input())
 
 dp = [[0 for _ in range(n + 1)] for _ in range(n + 1)]
 
+dp[2][0] = 1
+dp[2][1] = 2
+
 dp[3][0] = 1
 dp[3][1] = 3
-dp[3][2] = 0
-dp[3][3] = 0
 
-dp[4][0] = 1
-dp[4][1] = 4
-dp[4][2] = 2
-
-for i in range(5, n + 1):
+for i in range(4, n + 1):
     for j in range(k + 1):
         dp[i][j] = dp[i - 2][j - 1] + dp[i - 1][j]
 
