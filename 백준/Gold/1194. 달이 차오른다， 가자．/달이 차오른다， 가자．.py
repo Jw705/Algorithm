138a571c75690a279fea6b6ab94b5578a34cff41
Juwan Jeong <86391351+Jw705@users.noreply.github.com>
2024-07-03 12:07:08 +0900

    [Gold I] Title: 달이 차오른다, 가자., Time: 108 ms, Memory: 34436 KB -BaekjoonHub

--- 백준/Gold/1194. 달이 차오른다， 가자．/달이 차오른다， 가자．.py
+++ 백준/Gold/1194. 달이 차오른다， 가자．/달이 차오른다， 가자．.py
@@ -6,11 +6,14 @@ input = sys.stdin.readline
 
 def bfs(graph, visited, start):
     startX, startY = start
-    queue = deque([[startX, startY, 0, 0, 0, 0, 0, 0]])
-    visited[startX][startY][0][0][0][0][0][0] = 0
+    queue = deque([[startX, startY, 0]])
+    visited[startX][startY][0] = 0
+
+    key_dict = {'a': 1, 'b': 2, 'c': 4, 'd': 8, 'e': 16, 'f': 32}
+    door = {'A': 1, 'B': 2, 'C': 4, 'D': 8, 'E': 16, 'F': 32}
 
     while queue:
-        x, y, a, b, c, d, e, f = queue.popleft()
+        x, y, key = queue.popleft()
         dx = [-1, 1, 0, 0]
         dy = [0, 0, -1, 1]
 
@@ -19,53 +22,33 @@ def bfs(graph, visited, start):
             ny = y + dy[i]
             if nx < 0 or nx >= n or ny < 0 or ny >= m:
                 continue
-            elif visited[nx][ny][a][b][c][d][e][f] == -1:
-
+            elif visited[nx][ny][key] == -1:
                 # 출구 만나면 이동거리 반환
                 if graph[nx][ny] == '1':
-                    visited[nx][ny][a][b][c][d][e][f] = visited[x][y][a][b][c][d][e][f] + 1
-                    return visited[nx][ny][a][b][c][d][e][f]
+                    visited[nx][ny][key] = visited[x][y][key] + 1
+                    return visited[nx][ny][key]
 
                 # 키 만나면
                 elif graph[nx][ny] in ['a', 'b', 'c', 'd', 'e', 'f']:
-                    if graph[nx][ny] == 'a':
-                        visited[nx][ny][1][b][c][d][e][f] = visited[x][y][a][b][c][d][e][f] + 1
-                        queue.append([nx, ny, 1, b, c, d, e, f])
-                    elif graph[nx][ny] == 'b':
-                        visited[nx][ny][a][1][c][d][e][f] = visited[x][y][a][b][c][d][e][f] + 1
-                        queue.append([nx, ny, a, 1, c, d, e, f])
-                    elif graph[nx][ny] == 'c':
-                        visited[nx][ny][a][b][1][d][e][f] = visited[x][y][a][b][c][d][e][f] + 1
-                        queue.append([nx, ny, a, b, 1, d, e, f])
-                    elif graph[nx][ny] == 'd':
-                        visited[nx][ny][a][b][c][1][e][f] = visited[x][y][a][b][c][d][e][f] + 1
-                        queue.append([nx, ny, a, b, c, 1, e, f])
-                    elif graph[nx][ny] == 'e':
-                        visited[nx][ny][a][b][c][d][1][f] = visited[x][y][a][b][c][d][e][f] + 1
-                        queue.append([nx, ny, a, b, c, d, 1, f])
-                    elif graph[nx][ny] == 'f':
-                        visited[nx][ny][a][b][c][d][e][1] = visited[x][y][a][b][c][d][e][f] + 1
-                        queue.append([nx, ny, a, b, c, d, e, 1])
+                    updated_key = key | key_dict[graph[nx][ny]]
+                    visited[nx][ny][updated_key] = visited[x][y][key] + 1
+                    queue.append([nx, ny, updated_key])
 
                 # 문을 만나면
                 elif graph[nx][ny] in ['A', 'B', 'C', 'D', 'E', 'F']:
-                    visited[nx][ny][a][b][c][d][e][f] = visited[x][y][a][b][c][d][e][f] + 1
+                    visited[nx][ny][key] = visited[x][y][key] + 1
                     # 그 문에 맞는 열쇠가 있을 때만 해당 좌표를 큐에 추가
-                    if ((graph[nx][ny] == 'A' and a == 1) or (graph[nx][ny] == 'B' and b == 1) or (
-                            graph[nx][ny] == 'C' and c == 1)
-                            or (graph[nx][ny] == 'D' and d == 1) or (graph[nx][ny] == 'E' and e == 1) or (
-                                    graph[nx][ny] == 'F' and f == 1)):
-                        queue.append([nx, ny, a, b, c, d, e, f])
+                    if key & key_dict[graph[nx][ny].lower()] == key_dict[graph[nx][ny].lower()]:
+                        queue.append([nx, ny, key])
 
                 elif graph[nx][ny] == '.':
-                    queue.append([nx, ny, a, b, c, d, e, f])
-                    visited[nx][ny][a][b][c][d][e][f] = visited[x][y][a][b][c][d][e][f] + 1
+                    queue.append([nx, ny, key])
+                    visited[nx][ny][key] = visited[x][y][key] + 1
 
 
 n, m = map(int, input().split())
 graph = []
-visited = [[[[[[[[-1 for _ in range(2)] for _ in range(2)] for _ in range(2)]
-               for _ in range(2)] for _ in range(2)] for _ in range(2)] for _ in range(m)] for _ in range(n)]
+visited = [[[-1] * 64 for _ in range(m)] for _ in range(n)]
 
 minsik_locate = [0, 0]
 
