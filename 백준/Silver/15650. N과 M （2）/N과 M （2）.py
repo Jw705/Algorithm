0f341114a6a533c03f4a93fd0b4e4c5c21d8729a
정주완 <86391351+Jw705@users.noreply.github.com>
2024-09-02 11:31:12 +0900

    [Silver III] Title: N과 M (2), Time: 32 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Silver/15650. N과 M （2）/N과 M （2）.py
+++ 백준/Silver/15650. N과 M （2）/N과 M （2）.py
@@ -10,17 +10,13 @@ def find_sequence(arr):
         for i in range(1, n + 1):
             find_sequence([i])
     elif len(arr) == m:
-        for a in arr:
-            print(a, end=' ')
-        print('')
+        print(*arr)
     else:
         last = arr[len(arr) - 1]
         for j in range(last + 1, n + 1):
-            new_arr = []
-            for a in arr:
-                new_arr.append(a)
-            new_arr.append(j)
-            find_sequence(new_arr)
+            arr.append(j)
+            find_sequence(arr)
+            arr.pop()
 
 
 find_sequence([])
