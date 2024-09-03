0c99c70f9042e8a726ea3dccab97870b8ebedf0a
정주완 <86391351+Jw705@users.noreply.github.com>
2024-09-03 09:52:28 +0900

    [Silver III] Title: N과 M (4), Time: 44 ms, Memory: 31120 KB -BaekjoonHub

---
+++ 백준/Silver/15652. N과 M （4）/N과 M （4）.py
@@ -0,0 +1,20 @@
+import sys
+
+input = sys.stdin.readline
+
+n, m = map(int, input().split())
+
+
+def find_sequence(arr):
+    if len(arr) == m:
+        print(*arr)
+    else:
+        last = arr[len(arr) - 1]
+        for j in range(last, n + 1):
+            arr.append(j)
+            find_sequence(arr)
+            arr.pop()
+
+
+for i in range(1, n + 1):
+    find_sequence([i])
