90493b61c87070ac4c189b4014361923abb5a967
정주완 <86391351+Jw705@users.noreply.github.com>
2024-09-10 11:31:44 +0900

    [Silver I] Title: 카잉 달력, Time: 4584 ms, Memory: 31252 KB -BaekjoonHub

--- 백준/Silver/6064. 카잉 달력/카잉 달력.py
+++ 백준/Silver/6064. 카잉 달력/카잉 달력.py
@@ -1,36 +1,20 @@
 import sys
 
-# 입력 받기
 input = sys.stdin.readline
 
 t = int(input())
 
 for _ in range(t):
     m, n, x, y = map(int, input().split())
-    a = 1
-    b = 1
-    answer = 1
-    visited = [False] * (n + 1)
-    while a < x:
-        a += 1
-        if b < n:
-            b += 1
-        else:
-            b = 1
-        answer += 1
 
-    while True:
-        isKaingCalendarEnd = a == m and b == n
-        isTargetYear = a == x and b == y
-        if isTargetYear:
+    answer = x
+    b = (x - 1) % n + 1  # 'x' 해에 맞는 'b' 값 계산
+
+    while answer <= m * n:
+        if b == y:  # 목표 연도 도달
             print(answer)
             break
-        elif answer > m * n:
-            print(-1)
-            break
-        else:
-
-            b = (b + m) % n
-            if b == 0:
-                b = n
-            answer += m
+        answer += m
+        b = (b + m - 1) % n + 1
+    else:
+        print(-1)
