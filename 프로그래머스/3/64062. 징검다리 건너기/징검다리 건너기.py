b0f1ab8256843740d82e05f91ee0727f8f3814bd
정주완 <86391351+Jw705@users.noreply.github.com>
2024-09-11 11:10:41 +0900

    [level 3] Title: 징검다리 건너기, Time: 105.87 ms, Memory: 18.5 MB -BaekjoonHub

--- 프로그래머스/3/64062. 징검다리 건너기/징검다리 건너기.py
+++ 프로그래머스/3/64062. 징검다리 건너기/징검다리 건너기.py
@@ -2,21 +2,24 @@ from collections import deque
 
 def solution(stones, k):
     answer = 200000001
-    queue = deque()  # 현재 윈도우에서 유효한 값들의 인덱스를 저장할 deque
+    
+    # 현재 범위에서 유효한 값들의 인덱스를 저장할 queue
+    # queue의 맨 앞 값이 현재 범위의 최댓값이 되도록 유지한다.
+    queue = deque() 
     
     for i in range(len(stones)):
-        # 현재 인덱스가 k를 넘어가면 윈도우 밖의 값을 deque에서 제거
+        # 현재 범위 밖의 값은 queue에서 제거
         if queue and queue[0] < i - k + 1:
             queue.popleft()
         
-        # 현재 값이 deque에 있는 값들보다 크면 필요 없는 값들을 제거
+        # queue에 현재 값보다 작은 값이 있으면 제거
         while queue and stones[queue[-1]] <= stones[i]:
             queue.pop()
         
-        # 현재 인덱스를 deque에 추가
+        # 현재 인덱스를 queue에 추가
         queue.append(i)
         
-        # 윈도우가 k 크기에 도달하면 최댓값 갱신
+        # 최댓값 갱신
         if i >= k - 1:
             answer = min(answer, stones[queue[0]])
     
