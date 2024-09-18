99dd192abd777238e7dc7e46697184cd20e8e86b
정주완 <86391351+Jw705@users.noreply.github.com>
2024-09-18 12:45:23 +0900

    [level 3] Title: [1차] 추석 트래픽, Time: 511.47 ms, Memory: 10.7 MB -BaekjoonHub

--- 프로그래머스/3/17676. ［1차］ 추석 트래픽/［1차］ 추석 트래픽.py
+++ 프로그래머스/3/17676. ［1차］ 추석 트래픽/［1차］ 추석 트래픽.py
@@ -1,34 +1,37 @@
+#58m
+
 def solution(lines):
-    answer = 0
-    new_lines = []
-    tmp_arr = []
+    answer = 0    
+    updated_lines = [] # 로그의 시작시간과 끝시간 쌍을 저장할 배열
+    time_points = [] # 모든 로그의 시작시간, 끝시간을 저장할 배열
     
+    # 입력 데이터 전처리
     for line in lines:
-        # 입력 데이터 전처리
-        S1, S2, T = line.split()
-        T = int(float(T[0:-1])*1000) - 1
-        S3 = S2.split(":")
+        _, end_time_str, T_str = line.split()
         
-        end_time = int(S3[0])*60*60*1000 + int(S3[1])*60*1000 + int(float(S3[2])*1000)
-        start_time = end_time - T
+        # 응답완료시간(=끝시간)을 밀리초로 변환 (날짜는 고려하지 않음)
+        h, m, s = end_time_str.split(":")        
+        end_time = int(h)*60*60*1000 + int(m)*60*1000 + int(float(s)*1000)
         
-        new_lines.append([start_time, end_time])
-        tmp_arr.append(start_time)
-        tmp_arr.append(end_time)
+        # 처리시간을 밀리초로 변환
+        T = int(float(T_str[:-1])*1000)
         
-    new_lines.sort()    
-    tmp_arr.sort()
+        # 시작시간을 계산 (처리시간은 시작시간과 끝시간을 포함)
+        start_time = end_time - T + 1
+        
+        updated_lines.append([start_time, end_time])
+        time_points.append(start_time)
+        time_points.append(end_time)        
     
-    for time in tmp_arr:
-        a = time
-        b = a+999
-        cnt = 0
-        for j in range(len(new_lines)):
-            s,e = new_lines[j]
-            if a<=s<=b or a<=e<=b or(s<=a and b<=e):
-                cnt+=1
-        answer = max(cnt,answer)
-                
+    # 모든 시작시간, 끝시간에 대해 1초 구간(윈도우)을 만들어 처리 초당 최대 처리량 계산
+    for time in time_points:
+        window_start = time
+        window_end = window_start + 999
+        count = 0
+        for start, end in updated_lines:
+            if start <= window_end and end >= window_start:
+                count += 1
+        answer = max(count,answer)           
         
         
     return answer
