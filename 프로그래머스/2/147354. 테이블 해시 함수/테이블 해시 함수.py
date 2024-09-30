fb43b96257c35de25a1f8a6d247df6fd0832f2b4
정주완 <86391351+Jw705@users.noreply.github.com>
2024-09-30 18:20:56 +0900

    [level 2] Title: 테이블 해시 함수, Time: 40.97 ms, Memory: 64.2 MB -BaekjoonHub

--- 프로그래머스/2/147354. 테이블 해시 함수/테이블 해시 함수.py
+++ 프로그래머스/2/147354. 테이블 해시 함수/테이블 해시 함수.py
@@ -1,14 +1,11 @@
 def solution(data, col, row_begin, row_end):
     answer = 0
-    data.sort(key=lambda x:x[0],reverse=True)
-    data.sort(key=lambda x:x[col-1])
-    S=[]
+    data.sort(key=lambda x : (x[col-1], -x[0]))
+    
     for i in range(row_begin, row_end+1):
         S_i = 0
         for d in data[i-1]:
             S_i += d % i
-        S.append(S_i)
-    answer = S[0]
-    for i in range(1,len(S)):
-        answer = answer^S[i]
+        answer = answer^S_i
+        
     return answer
