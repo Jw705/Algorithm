554cebe45696ee1be72791cf83910ddcb08ae903
정주완 <86391351+Jw705@users.noreply.github.com>
2024-10-01 16:54:46 +0900

    [level 2] Title: 우박수열 정적분, Time: 6.17 ms, Memory: 12.4 MB -BaekjoonHub

---
+++ 프로그래머스/2/134239. 우박수열 정적분/우박수열 정적분.py
@@ -0,0 +1,25 @@
+def solution(k, ranges):
+    ubak = [k]
+    while k > 1:
+        if k % 2 == 0:
+            k //= 2
+            ubak.append(k)
+        else:
+            k = k * 3 + 1
+            ubak.append(k)
+    
+    integral = []
+    for i in range(len(ubak)-1):
+        area = min(ubak[i],ubak[i + 1]) + ((abs(ubak[i]-ubak[i+1]) / 2.0))
+        integral.append(area)
+    
+    answer = []
+    for r in ranges:
+        a = r[0]
+        b = len(ubak) + r[1] - 1
+        if a > b:
+            answer.append(-1.0)
+        else:
+            answer.append(sum(integral[a:b]))
+        
+    return answer
