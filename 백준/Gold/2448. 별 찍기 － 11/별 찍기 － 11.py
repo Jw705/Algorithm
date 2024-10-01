5e8537c2bd29f92bb3967ce9245221ff18269f5c
정주완 <86391351+Jw705@users.noreply.github.com>
2024-10-01 16:03:49 +0900

    [Gold IV] Title: 별 찍기 - 11, Time: 2976 ms, Memory: 286660 KB -BaekjoonHub

--- 백준/Gold/2448. 별 찍기 － 11/별 찍기 － 11.py
+++ 백준/Gold/2448. 별 찍기 － 11/별 찍기 － 11.py
@@ -5,45 +5,42 @@ input = sys.stdin.readline
 n = int(input())
 
 answer = []
-t = ""
-for _ in range(n - 1):
-    t += " "
-t += "*"
-for _ in range(n - 1):
-    t += " "
-print(t)
+t = [" "] * (n - 1) + ["*"] + [" "] * (n - 1)
+print("".join(t))
 answer.append(t)
 
 for i in range(n - 1):
     recent_layer = answer[-1]
-    new_layer = ""
+    new_layer = []
 
     j = 0
     while j < len(recent_layer):
-        if recent_layer[j:j + 3] == "***":
-            new_layer = new_layer[:-1] + "*"
+        if recent_layer[j:j + 3] == ["*", "*", "*"]:
+            new_layer[-1] = "*"
             cnt = 0
             while j < len(recent_layer):
                 if recent_layer[j] == ' ':
                     if cnt > 0:
-                        new_layer = new_layer[:-1]
+                        new_layer.pop()
                         break
                     else:
-                        new_layer += ' '
+                        new_layer.append(' ')
                         cnt = 1
                 else:
                     cnt = 0
-                    new_layer += ' '
+                    new_layer.append(' ')
                 j += 1
-            new_layer += '*'
-        elif recent_layer[j:j + 3] == "* *":
-            new_layer = new_layer[:-1] + "*****"
+            new_layer.append('*')
+        elif recent_layer[j:j + 3] == ["*", " ", "*"]:
+            new_layer[-1] = "*"
+            new_layer.extend(["*", "*", "*", "*"])
             j += 4
         elif recent_layer[j] == "*":
-            new_layer = new_layer[:-1] + "* *"
+            new_layer[-1] = "*"
+            new_layer.extend([" ", "*"])
             j += 2
         else:
-            new_layer += recent_layer[j]
+            new_layer.append(recent_layer[j])
             j += 1
-    print(new_layer)
+    print("".join(new_layer))
     answer.append(new_layer)
