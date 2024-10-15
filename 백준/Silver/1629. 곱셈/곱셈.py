b9b55a04977b67dca72e2ee9d07ff7a219b609b8
정주완 <86391351+Jw705@users.noreply.github.com>
2024-10-15 11:08:53 +0900

    [Silver I] Title: 곱셈, Time: 36 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Silver/1629. 곱셈/곱셈.py
+++ 백준/Silver/1629. 곱셈/곱셈.py
@@ -5,14 +5,11 @@ input = sys.stdin.readline
 a, b, c = map(int, input().split())
 answer = 1
 
-def power(b):
-    if b == 0:
-        return 1
-    if b == 1:
-        return a % c
-    if b % 2 == 0:
-        return power(b // 2) ** 2 % c
-    else:
-        return power(b // 2) ** 2 * a % c
+while b > 0:
+    if (b % 2) == 1:
+        answer = answer * a % c
+    b = b >> 1
+    a = a * a
+    a = a % c
 
-print(power(b))
+print(answer)
