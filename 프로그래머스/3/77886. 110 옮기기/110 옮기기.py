a858713230779f256c4e9a0b4992c975d57322d2
정주완 <86391351+Jw705@users.noreply.github.com>
2024-10-21 11:44:22 +0900

    [level 3] Title: 110 옮기기, Time: 378.61 ms, Memory: 25.5 MB -BaekjoonHub

--- 프로그래머스/3/77886. 110 옮기기/110 옮기기.py
+++ 프로그래머스/3/77886. 110 옮기기/110 옮기기.py
@@ -1,13 +1,13 @@
+# 주어진 문자열에서 110을 제거
 def remove_110(string):
     stack = []
-    count_110 = 0
     for char in string:
         stack.append(char)
+        # 스택에 쌓인 개수가 3개 이상이고, 마지막 3개가 '110'이면
         if len(stack) >= 3 and ''.join(stack[-3:]) == '110':
             stack.pop()
             stack.pop()
             stack.pop()
-            count_110 += 1
     return ''.join(stack)
 
 
@@ -15,14 +15,14 @@ def solution(s):
     answer = []
     for string in s:
         str_remove_110 = remove_110(string)
+        # 제거된 110의 개수 계산
         count_110 = (len(string) - len(str_remove_110)) // 3
-
         idx = str_remove_110.rfind('0')
         if idx == -1:
-            # '0'이 없으면 문자열의 맨 앞에 삽입
+            # '0'이 없으면 문자열의 맨 앞에 110 모두 삽입
             answer.append('110' * count_110 + str_remove_110)
         else:
-            # '0'이 있으면 그 뒤에 110 삽입
+            # '0'이 있으면 그 뒤에 110 모두 삽입
             answer.append(str_remove_110[:idx + 1] + '110' * count_110 + str_remove_110[idx + 1:])
 
     return answer
