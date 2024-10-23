44cfcdbd0b4cf74de0143a5c3a8e034cfb2dcf9f
정주완 <86391351+Jw705@users.noreply.github.com>
2024-10-23 11:24:20 +0900

    [level 3] Title: 스타 수열, Time: 782.85 ms, Memory: 75.8 MB -BaekjoonHub

--- 프로그래머스/3/70130. 스타 수열/스타 수열.py
+++ 프로그래머스/3/70130. 스타 수열/스타 수열.py
@@ -1,20 +1,20 @@
+# 40m
+
 def solution(a):
-    
-    num_dict = dict()  
-    
+    seq_info = {}
+
     for i in range(len(a)):
-        if not a[i] in num_dict:
-            num_dict[a[i]] = [0,-1]
-            
-        if i > 0 and num_dict[a[i]][1] < i - 1 and a[i - 1] != a[i]:
-            num_dict[a[i]][0] += 2
-            num_dict[a[i]][1] = i
-        elif i < len(a) - 1 and num_dict[a[i]][1] < i and a[i + 1] != a[i]:
-            num_dict[a[i]][0] += 2
-            num_dict[a[i]][1] = i + 1
-    
-    answer = 0
-    for max_star_sequence_length, a in num_dict.values():
-        answer = max(answer, max_star_sequence_length)
-    
-    return answer
+        if a[i] not in seq_info:
+            # a[i]를 교집합으로 하는 스타수열 정보 [스타 수열 길이, 마지막으로 스타수열을 만드는 인덱스]
+            seq_info[a[i]] = [0, -1]
+
+        if i > 0 and seq_info[a[i]][1] < i - 1 and a[i - 1] != a[i]:
+            # [ a[i-1], [a[i] ]를 스타 수열에 추가할 수 있다면
+            seq_info[a[i]][0] += 2
+            seq_info[a[i]][1] = i
+        elif i < len(a) - 1 and seq_info[a[i]][1] < i and a[i + 1] != a[i]:
+            # [ a[i], [a[i + 1] ]를 스타 수열에 추가할 수 있다면
+            seq_info[a[i]][0] += 2
+            seq_info[a[i]][1] = i + 1
+
+    return max(star_sequence_length for star_sequence_length, _ in seq_info.values())
