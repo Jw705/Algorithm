007a581d00c2d338d57b4aba7be08fe9a091030a
정주완 <86391351+Jw705@users.noreply.github.com>
2024-10-29 11:34:58 +0900

    [level 3] Title: n + 1 카드게임, Time: 41.72 ms, Memory: 10.3 MB -BaekjoonHub

--- 프로그래머스/3/258707. n ＋ 1 카드게임/n ＋ 1 카드게임.py
+++ 프로그래머스/3/258707. n ＋ 1 카드게임/n ＋ 1 카드게임.py
@@ -1,65 +1,58 @@
+# 30m
+
 def solution(coin, cards):
     n = len(cards)
     cur_idx = n // 3
-    origin_cards = dict()
-    new_cards = dict()
-    
-    for i in range(cur_idx):
-        origin_cards[cards[i]] = True
-    
-    answer = 0
-    while True:
-        answer += 1
-        
+
+    origin_cards = set(cards[:cur_idx])
+    new_cards = set()
+
+    answer = 1
+    while cur_idx + 2 <= n:
         # 카드 두장 뽑기
-        if cur_idx + 2 > n:
-            break
-        else:
-            for i in range(cur_idx, cur_idx + 2):
-                new_cards[cards[i]] = True
-            cur_idx += 2
-        
+        for i in range(cur_idx, cur_idx + 2):
+            new_cards.add(cards[i])
+        cur_idx += 2
+
         # 카드 두 장으로 n+1 만들기
         # origin_cards로 가능한지 확인
         is_possible = False
-        for i in range(1000):
-            if i in origin_cards:
-                if (n + 1) - i in origin_cards:
-                    origin_cards.pop(i)
-                    origin_cards.pop((n + 1) - i)
-                    is_possible = True
-                    break
-                    
+        
+        # 가장 먼저, 코인을 사용하지 않는 경우 확인
+        for i in range(n + 1):
+            if i in origin_cards and (n + 1) - i in origin_cards:
+                origin_cards.remove(i)
+                origin_cards.remove((n + 1) - i)
+                is_possible = True
+                break
+        # 코인 사용 없이 불가능하면 코인 하나만 사용하는 경우 확인
         if not is_possible and coin >= 1:
-            for i in range(1000):
-                if i in origin_cards:
-                    if (n + 1) - i in new_cards:
-                        origin_cards.pop(i)
-                        new_cards.pop((n + 1) - i)
-                        is_possible = True
-                        coin -= 1
-                        break
-                elif i in new_cards:
-                    if (n + 1) - i in origin_cards:
-                        new_cards.pop(i)
-                        origin_cards.pop((n + 1) - i)
-                        is_possible = True
-                        coin -= 1
-                        break           
-                        
+            for i in range(n + 1):
+                if i in origin_cards and (n + 1) - i in new_cards:
+                    origin_cards.remove(i)
+                    new_cards.remove((n + 1) - i)
+                    is_possible = True
+                    coin -= 1
+                    break
+                elif i in new_cards and (n + 1) - i in origin_cards:
+                    new_cards.remove(i)
+                    origin_cards.remove((n + 1) - i)
+                    is_possible = True
+                    coin -= 1
+                    break
+        # 코인 두개를 사용하는 경우 확인
         if not is_possible and coin >= 2:
-            for i in range(1000):
-                if i in new_cards:
-                    if (n + 1) - i in new_cards:
-                        new_cards.pop(i)
-                        new_cards.pop((n + 1) - i)
-                        is_possible = True
-                        coin -= 2
-                        break            
-        
+            for i in range(n + 1):
+                if i in new_cards and (n + 1) - i in new_cards:
+                    new_cards.remove(i)
+                    new_cards.remove((n + 1) - i)
+                    is_possible = True
+                    coin -= 2
+                    break
+
         if not is_possible:
-            break     
-        
-        
-    
+            break
+
+        answer += 1
+
     return answer
