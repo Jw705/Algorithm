b68c69057d3e10b2b3f2220fac0a7098ef29fc0c
정주완 <86391351+Jw705@users.noreply.github.com>
2024-11-13 10:28:35 +0900

    [level 3] Title: 선입 선출 스케줄링, Time: 12.94 ms, Memory: 10.3 MB -BaekjoonHub

--- 프로그래머스/3/12920. 선입 선출 스케줄링/README.md
+++ 프로그래머스/3/12920. 선입 선출 스케줄링/README.md
@@ -1,10 +1,10 @@
 # [level 3] 선입 선출 스케줄링 - 12920 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/12920) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/12920#) 
 
 ### 성능 요약
 
-메모리: 10.4 MB, 시간: 13.31 ms
+메모리: 10.3 MB, 시간: 12.94 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2024년 11월 13일 10:16:52
+2024년 11월 13일 10:28:34
 
 ### 문제 설명
 
--- 프로그래머스/3/12920. 선입 선출 스케줄링/선입 선출 스케줄링.py
+++ 프로그래머스/3/12920. 선입 선출 스케줄링/선입 선출 스케줄링.py
@@ -1,34 +1,38 @@
-def find_finished_task(time, cores):
+# 주어진 시간 동안 모든 코어가 완료한 작업 수 계산
+def cal_finished_task(time, cores):
     finished_task = len(cores)
     for core in cores:
         finished_task += (time // core)
     return finished_task
-    
+
 
 def solution(n, cores):
     answer = 0
-    time = -1
-    
-    l = 0
-    r = 50000 * 10000
-    while l < r:        
-        mid = (l + r) // 2
-        finished_task = find_finished_task(mid, cores)
-        #print([l,r], mid, finished_task)
+    finished_time = -1
+
+    # 파라메트릭 서치로 모든 작업이 끝나는 시간 (time) 계산
+    left = 0
+    right = 25000 * 10000
+    while left <= right:
+        mid = (left + right) // 2
+        finished_task = cal_finished_task(mid, cores)
         if finished_task >= n:
-            time = mid
-            r = mid
+            finished_time = mid
+            right = mid - 1
         else:
-            l = mid + 1
-            
-    
-    left_task = n - find_finished_task(time - 1, cores)  
-    for i in range(len(cores)):
-        if time % cores[i] == 0:  
-            left_task -= 1
-        if left_task == 0:
-            answer = (i + 1)
+            left = mid + 1
+
+    # 작업이 끝나는 시간에 처리해야 하는 일의 개수 (remaining_tasks) 계산
+    remaining_tasks = n - cal_finished_task(finished_time - 1, cores)
+
+    # cores를 앞에서 부터 확인하면서, 마지막 작업을 처리하는 core 확인
+    for i, core in enumerate(cores):
+        # finished_time에 core가 작업이 끝난 상태인 경우
+        if finished_time % core == 0:
+            remaining_tasks -= 1
+        # 남은 작업이 없으면 현재 core의 순서가 정답
+        if remaining_tasks == 0:
+            answer = i + 1
             break
-    
-       
+
     return answer
