a65d58ef4eaae011427bbe8dc2630a59f62b6fce
정주완 <86391351+Jw705@users.noreply.github.com>
2024-11-14 10:09:28 +0900

    [level 3] Title: 풍선 터트리기, Time: 872.26 ms, Memory: 64 MB -BaekjoonHub

--- 프로그래머스/3/68646. 풍선 터트리기/풍선 터트리기.py
+++ 프로그래머스/3/68646. 풍선 터트리기/풍선 터트리기.py
@@ -1,24 +1,25 @@
+# 32m
+
 def solution(a):
     answer = 0
-    min_start_left = [0] * len(a)
-    min_start_right = [0] * len(a)
+    n = len(a)
+
+    # min_start_left[i]는 a[0:i + 1] 중 최소값 저장
+    # min_start_right[i]는 a[i:len(a)] 중 최소값 저장
+    min_start_left = [0] * n
+    min_start_right = [0] * n
+
     min_start_left[0] = a[0]
-    min_start_right[len(a) - 1] = a[len(a) - 1]
-    for i in range(1, len(a)):
+    min_start_right[n - 1] = a[n - 1]
+    for i in range(1, n):
         min_start_left[i] = min(a[i], min_start_left[i - 1])
-    for i in range(len(a)-2, -1, -1):
+    for i in range(n - 2, -1, -1):
         min_start_right[i] = min(a[i], min_start_right[i + 1])
-    
-    for i in range(len(a)):
-        small_num_cnt = 0
-        if min_start_left[i] < a[i]:
-            small_num_cnt += 1
-        if min_start_right[i] < a[i]:
-            small_num_cnt += 1
-        #print([min_start_left[i], a[i], min_start_right[i]], small_num_cnt)
-        if small_num_cnt < 2:
-            answer += 1
-    
-    #print(min_start_left)
-    #print(min_start_right)
+
+    # 자신 왼쪽과 오른쪽에 자신보다 작은 수가 있으면 마지막까지 남길 수 없다.
+    for i in range(n):
+        if min_start_left[i] < a[i] and min_start_right[i] < a[i]:
+            continue
+        answer += 1
+
     return answer
