b9d96fd5890fb3109e0660b826677b7115aa5016
정주완 <86391351+Jw705@users.noreply.github.com>
2024-11-15 16:51:00 +0900

    [level 3] Title: 코딩 테스트 공부, Time: 353.77 ms, Memory: 18 MB -BaekjoonHub

--- 프로그래머스/3/118668. 코딩 테스트 공부/코딩 테스트 공부.py
+++ 프로그래머스/3/118668. 코딩 테스트 공부/코딩 테스트 공부.py
@@ -1,28 +1,27 @@
-import math
+# 80m
 
 def solution(alp, cop, problems):
-    max_alp_req, max_cop_req = [0, 0]  # 목표값
-    
+    target_alp, target_cop = [0, 0]  # 목표값
+
     for problem in problems:
-        max_alp_req = max(max_alp_req, problem[0])
-        max_cop_req = max(max_cop_req, problem[1])
-        
+        target_alp = max(target_alp, problem[0])
+        target_cop = max(target_cop, problem[1])
+
     dp = [[int(1e9) for _ in range(455)] for _ in range(455)]
-    alp = min(alp, max_alp_req)  # 둘중 하나라도 목표값을 넘어가면 안된다.
-    cop = min(cop, max_cop_req)
+    alp = min(alp, target_alp)  # 둘중 하나라도 목표값을 넘어가면 안된다.
+    cop = min(cop, target_cop)
     dp[alp][cop] = 0
-    
-    for i in range(alp, max_alp_req + 1):
-        for j in range(cop, max_cop_req + 1):
-            dp[i+1][j] = min(dp[i+1][j], dp[i][j] + 1)
-            dp[i][j+1] = min(dp[i][j+1], dp[i][j] + 1)
-            
+
+    for i in range(alp, target_alp + 1):
+        for j in range(cop, target_cop + 1):
+            dp[i + 1][j] = min(dp[i + 1][j], dp[i][j] + 1)
+            dp[i][j + 1] = min(dp[i][j + 1], dp[i][j] + 1)
+
             for problem in problems:
                 alp_req, cop_req, alp_rwd, cop_rwd, cost = problem
-                if i >= alp_req and j >= cop_req:                    
-                    new_alp = min(i+alp_rwd, max_alp_req)
-                    new_cop = min(j+cop_rwd, max_cop_req)
+                if i >= alp_req and j >= cop_req:
+                    new_alp = min(i + alp_rwd, target_alp)
+                    new_cop = min(j + cop_rwd, target_cop)
                     dp[new_alp][new_cop] = min(dp[new_alp][new_cop], dp[i][j] + cost)
-            
-        
-    return dp[max_alp_req][max_cop_req]
+
+    return dp[target_alp][target_cop]
