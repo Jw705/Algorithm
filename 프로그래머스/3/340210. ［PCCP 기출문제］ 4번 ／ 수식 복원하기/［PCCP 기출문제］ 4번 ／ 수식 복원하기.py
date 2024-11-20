99a8df28a79ce123e69a02426b983df35eb4198c
정주완 <86391351+Jw705@users.noreply.github.com>
2024-11-20 10:51:40 +0900

    [level 3] Title: [PCCP 기출문제] 4번 / 수식 복원하기, Time: 0.89 ms, Memory: 10.5 MB -BaekjoonHub

--- 프로그래머스/3/340210. ［PCCP 기출문제］ 4번 ／ 수식 복원하기/［PCCP 기출문제］ 4번 ／ 수식 복원하기.py
+++ 프로그래머스/3/340210. ［PCCP 기출문제］ 4번 ／ 수식 복원하기/［PCCP 기출문제］ 4번 ／ 수식 복원하기.py
@@ -1,9 +1,13 @@
+# 58m
+
+# 10진수를 N진수로 변환
 def base10toN(number, N):
     if number == 0:
-        return '0'
-    
+        return '0'    
     result = ''
     while number > 0:
+        # number를 N으로 나눈 나머지를 구하고
+        # 그 값을 문자열로 변환하여 result 앞에 추가
         result = str(number % N) + result
         number //= N
     return result
@@ -14,6 +18,7 @@ def solution(expressions):
     parsed_expressions = []
     base = [False] * 2 + [True] * 8
     
+    # 문자열 파싱 및 불가능한 진법 처리
     for expression in expressions:
         A, operator, B, _ , C = expression.split()
                     
@@ -23,28 +28,29 @@ def solution(expressions):
             max_num = max(max(A), max(B))
         else:
             parsed_expressions.append([A, operator, B, C])
-        
+        # n 이하의 진법은 불가능
         for i in range(2, int(max_num) + 1):
             if base[i] == True:
                 base[i] = False
     
+    # 수식을 확인하며 불가능한 진법 확인
     for A, operator, B, C in parsed_expressions:
         for i in range(2, 10):
             if base[i] == False:
                 continue            
             new_A, new_B, new_C = int(A, i), int(B, i), int(C, i)
-            #print(i, [A, operator, B, C], [new_A, operator, new_B, new_C])
             if operator == '+' and new_A + new_B != new_C:
                 base[i] = False
             elif operator == '-' and new_A - new_B != new_C:
                 base[i] = False
     
+    # 가능한 진법만 pos_base에 저장
     pos_base = []
     for i in range(2, 10):
         if base[i] == True:
             pos_base.append(i)
-    #print(pos_base)
     
+    # 정답 계산
     answer = []
     for A, operator, B in questions:
         if operator == '+':
@@ -52,17 +58,15 @@ def solution(expressions):
         else:
             C = base10toN(int(A, pos_base[0]) - int(B, pos_base[0]), pos_base[0])
             
-        #print([A, operator, B, C])
         for i in pos_base:
             if operator == '+':
                 tmp_C = base10toN(int(A, i) + int(B, i), i)
             else:
                 tmp_C = base10toN(int(A, i) - int(B, i), i)
-                
+            # 결과값이 다른 값이 있으면 C = '?'
             if tmp_C != C:
                 C = '?'
                 break
-        #print([A, operator, B, C])
         answer.append(f'{A} {operator} {B} = {C}')
         
     return answer
