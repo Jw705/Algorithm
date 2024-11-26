3c407fbe77464ddbc12da4af3495a4abb2024296
정주완 <86391351+Jw705@users.noreply.github.com>
2024-11-26 14:23:00 +0900

    [Gold II] Title: 가장 긴 증가하는 부분 수열 2, Time: 776 ms, Memory: 157604 KB -BaekjoonHub

--- 백준/Gold/12015. 가장 긴 증가하는 부분 수열 2/가장 긴 증가하는 부분 수열 2.py
+++ 백준/Gold/12015. 가장 긴 증가하는 부분 수열 2/가장 긴 증가하는 부분 수열 2.py
@@ -1,31 +1,22 @@
 import sys
+from bisect import bisect_left
 
 input = sys.stdin.readline
 
 n = int(input())
 data = list(map(int, input().split()))
-dp = [0]
 
-
-# target 보다 작은 값 중 가장 큰 값의 index 반환
-def binary_left(array, target):
-    start, end = 0, len(array) - 1
-    result = -1
-    while start <= end:
-        mid = (start + end) // 2
-        if array[mid] < target:
-            result = mid
-            start = mid + 1
+def LIS(array):
+    dp = []  # dp 리스트를 빈 리스트로 초기화
+    for num in array:
+        # num이 들어갈 위치 찾기
+        idx = bisect_left(dp, num)
+        # num이 가장 큰 값이면 dp에 추가
+        if idx == len(dp):
+            dp.append(num)
         else:
-            end = mid - 1
-    return result
+            # num이 들어갈 위치의 값을 num으로 대체
+            dp[idx] = num
+    return len(dp)
 
-
-for i in range(len(data)):
-    idx = binary_left(dp, data[i]) + 1
-    if idx >= len(dp):
-        dp.append(data[i])
-    else:
-        dp[idx] = min(dp[idx], data[i])
-
-print(len(dp) - 1)
+print(LIS(data))
