6c965bff7b1bf623cf794f98e48064b57d8b2335
정주완 <86391351+Jw705@users.noreply.github.com>
2024-11-28 09:24:46 +0900

    [level 3] Title: 표 편집, Time: 782.49 ms, Memory: 237 MB -BaekjoonHub

--- 프로그래머스/3/81303. 표 편집/표 편집.py
+++ 프로그래머스/3/81303. 표 편집/표 편집.py
@@ -1,41 +1,39 @@
 def solution(n, k, cmd):
-    # 연결 리스트: 각 행의 이전/다음 행을 저장
-    linked_list = {i: [i - 1, i + 1] for i in range(n)}
-    linked_list[0][0] = None  # 첫 행의 이전은 없음
-    linked_list[n - 1][1] = None  # 마지막 행의 다음은 없음
-    
-    del_stack = []  # 삭제된 행 스택
-    current = k  # 현재 선택된 행
+    linked_list = {i : [i - 1, i + 1] for i in range(n)}
+    linked_list[0][0] = None
+    linked_list[n - 1][1] = None
+    del_stack = []
     
     for c in cmd:
-        if c[0] == 'U':  # 'U X': 위로 X칸
+        if c[0] == 'U':
             X = int(c.split()[1])
             for _ in range(X):
-                current = linked_list[current][0]
-        elif c[0] == 'D':  # 'D X': 아래로 X칸
+                k = linked_list[k][0]
+        elif c[0] == 'D':
             X = int(c.split()[1])
             for _ in range(X):
-                current = linked_list[current][1]
-        elif c[0] == 'C':  # 'C': 현재 행 삭제
-            prev, next_ = linked_list[current]
-            del_stack.append((current, prev, next_))  # 삭제된 행 저장
-            
-            if prev is not None:
-                linked_list[prev][1] = next_  # 이전 행의 다음을 현재 행의 다음으로 연결
+                k = linked_list[k][1]
+        if c[0] == 'C':
+            del_stack.append(k)
+            prev, next_ = linked_list[k]
             if next_ is not None:
-                linked_list[next_][0] = prev  # 다음 행의 이전을 현재 행의 이전으로 연결
-            
-            # 현재 행 갱신
-            current = next_ if next_ is not None else prev
-        elif c[0] == 'Z':  # 'Z': 가장 최근 삭제된 행 복구
-            node, prev, next_ = del_stack.pop()
+                linked_list[next_][0] = prev
+                k = next_
+            else:
+                k = prev
             if prev is not None:
-                linked_list[prev][1] = node  # 이전 행과 복구된 행 연결
+                linked_list[prev][1] = next_
+        if c[0] == 'Z':
+            target = del_stack.pop()
+            prev, next_ = linked_list[target]
+            if prev is not None:
+                linked_list[prev][1] = target
             if next_ is not None:
-                linked_list[next_][0] = node  # 다음 행과 복구된 행 연결
+                linked_list[next_][0] = target
     
-    # 최종 상태 구성
-    result = ['O'] * n
-    for node, _, _ in del_stack:  # 삭제된 행은 'X'로 표시
-        result[node] = 'X'
-    return ''.join(result)
+    state = ['O'] * n
+    for deleted_low in del_stack:
+        state[deleted_low] = 'X'  
+    
+    answer = ''.join(state)    
+    return answer
