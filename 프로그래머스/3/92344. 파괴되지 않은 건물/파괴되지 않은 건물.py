abf1cfd58d42524642075598b7812203429d37fd
정주완 <86391351+Jw705@users.noreply.github.com>
2024-12-02 10:32:41 +0900

    [level 3] Title: 파괴되지 않은 건물, Time: 836.35 ms, Memory: 136 MB -BaekjoonHub

--- 프로그래머스/3/92344. 파괴되지 않은 건물/파괴되지 않은 건물.py
+++ 프로그래머스/3/92344. 파괴되지 않은 건물/파괴되지 않은 건물.py
@@ -1,34 +1,27 @@
 def solution(board, skill):
     n, m = len(board), len(board[0])
-    # 누적합 배열 생성
-    acc = [[0] * (m + 1) for _ in range(n + 1)]
-
-    # skill 적용
+    prifix_sum = [[0 for _ in range(m + 1)] for _ in range(n + 1)]
+    
     for type_, r1, c1, r2, c2, degree in skill:
-        if type_ == 1:  # 공격일 경우
-            degree = -degree
-        # 누적합 방식으로 시작점과 끝점 설정
-        acc[r1][c1] += degree
-        acc[r1][c2 + 1] -= degree
-        acc[r2 + 1][c1] -= degree
-        acc[r2 + 1][c2 + 1] += degree
-
-    # 누적합 적용 (행 기준)
+        if type_ == 1:
+            degree *= -1
+        prifix_sum[r1][c1] += degree
+        prifix_sum[r1][c2 + 1] -= degree
+        prifix_sum[r2 + 1][c1] -= degree
+        prifix_sum[r2 + 1][c2 + 1] += degree
+    
     for i in range(n):
         for j in range(m):
-            acc[i][j + 1] += acc[i][j]
-
-    # 누적합 적용 (열 기준)
-    for j in range(m):
-        for i in range(n):
-            acc[i + 1][j] += acc[i][j]
-
-    # 결과 계산
+            prifix_sum[i][j + 1] += prifix_sum[i][j]
+            
+    for i in range(m):
+        for j in range(n):
+            prifix_sum[j + 1][i] += prifix_sum[j][i]
+        
     answer = 0
     for i in range(n):
         for j in range(m):
-            board[i][j] += acc[i][j]
-            if board[i][j] > 0:
-                answer += 1
-
+            if board[i][j] + prifix_sum[i][j] > 0:
+                answer += 1       
+    
     return answer
