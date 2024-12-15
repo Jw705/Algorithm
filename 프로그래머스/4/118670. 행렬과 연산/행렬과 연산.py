9bbd8b7561e6757689ca406d3c6561dcdd256ea9
정주완 <86391351+Jw705@users.noreply.github.com>
2024-12-15 15:41:56 +0900

    [level 4] Title: 행렬과 연산, Time: 226.33 ms, Memory: 64.8 MB -BaekjoonHub

---
+++ 프로그래머스/4/118670. 행렬과 연산/행렬과 연산.py
@@ -0,0 +1,24 @@
+from collections import deque
+
+
+def solution(rc, operations):
+    n = len(rc)
+    m = len(rc[0])
+    left_col = deque([rc[i][0] for i in range(n)])
+    right_col = deque([rc[i][m - 1] for i in range(n)])
+    rows = deque([deque(rc[i][1:m - 1]) for i in range(n)])
+
+    for op in operations:
+        if op == 'ShiftRow':
+            left_col.appendleft(left_col.pop())
+            rows.appendleft(rows.pop())
+            right_col.appendleft(right_col.pop())
+        else:  # 'Rotate'
+            rows[0].appendleft(left_col.popleft())
+            right_col.appendleft(rows[0].pop())
+            rows[n - 1].append(right_col.pop())
+            left_col.append(rows[n - 1].popleft())
+    answer = []
+    for i in range(n):
+        answer.append([left_col[i]] + list(rows[i]) + [right_col[i]])
+    return answer
