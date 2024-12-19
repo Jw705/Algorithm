303d60cd807260034838445634f9d53030acacd4
정주완 <86391351+Jw705@users.noreply.github.com>
2024-12-19 11:11:46 +0900

    [level 3] Title: 산 모양 타일링, Time: 48.94 ms, Memory: 18.9 MB -BaekjoonHub

--- 프로그래머스/3/258705. 산 모양 타일링/산 모양 타일링.py
+++ 프로그래머스/3/258705. 산 모양 타일링/산 모양 타일링.py
@@ -1,7 +1,7 @@
 def solution(n, tops):
-    dp1 = [2 + tops[0]] + [0] * (n-1)
-    dp2 = [1] + [0] * (n-1)
+    dp1 = [2 + tops[0]] + [0] * (n - 1)
+    dp2 = [1] + [0] * (n - 1)
     for i in range(1, len(tops)):
-        dp1[i] = (dp1[i-1] * (2 + tops[i]) + dp2[i-1] * (1 + tops[i])) % 10007
-        dp2[i] = (dp2[i-1] + dp1[i-1]) % 10007    
+        dp1[i] = (dp1[i - 1] * (2 + tops[i]) + dp2[i - 1] * (1 + tops[i])) % 10007
+        dp2[i] = (dp2[i - 1] + dp1[i - 1]) % 10007    
     return (dp1[-1] + dp2[-1]) % 10007
