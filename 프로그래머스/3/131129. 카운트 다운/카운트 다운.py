e472b3adcb78b98ff55c152a0474ecca47f8c2f9
정주완 <86391351+Jw705@users.noreply.github.com>
2024-12-23 11:17:36 +0900

    [level 3] Title: 카운트 다운, Time: 1707.92 ms, Memory: 14.9 MB -BaekjoonHub

--- 프로그래머스/3/131129. 카운트 다운/카운트 다운.py
+++ 프로그래머스/3/131129. 카운트 다운/카운트 다운.py
@@ -1,40 +1,32 @@
+# 36m
+
 def solution(target):
     MAX = target + 1
-    dp = [MAX] * (target) + [0]
-    dp2 = [0] * (target) + [0]
+    dp = [MAX] * (target) + [0]  # dp[i]: i점을 만드는 최소 다트 수
+    dp2 = [0] * (target) + [0]  # dp2[i]: i점을 만드는 최소 다트 수 중 "싱글" 또는 "불"을 맞춘 횟수
     
+    single_and_bool = list(range(1,21)) + [50]
+    double_and_triple = {i * 2 for i in range(11, 21)} | {i * 3 for i in range(7, 21)}
+        
     for i in range(target, -1, -1):
-        # 불
-        if i >= 50:
-            j = 50
+        # 싱글, 불
+        for j in single_and_bool:
+            if j > i:
+                continue
             if dp[i - j] > dp[i] + 1:
                 dp[i - j] = dp[i] + 1
                 dp2[i - j] = dp2[i] + 1
             if dp[i - j] == dp[i] + 1:
-                dp2[i - j] = max(dp2[i - j], dp2[i] + 1)   
-        
-        for j in range(1, 21):
-            # 싱글
-            if i >= j:
-                if dp[i - j] > dp[i] + 1:
-                    dp[i - j] = dp[i] + 1
-                    dp2[i - j] = dp2[i] + 1
-                if dp[i - j] == dp[i] + 1:
-                    dp2[i - j] = max(dp2[i - j], dp2[i] + 1)         
-            # 더블
-            if i >= j * 2:
-                if dp[i - (j * 2)] > dp[i] + 1:
-                    dp[i - (j * 2)] = dp[i] + 1            
-                    dp2[i - (j * 2)] = dp2[i] 
-                if dp[i - (j * 2)] == dp[i] + 1:        
-                    dp2[i - (j * 2)] = max(dp2[i - (j * 2)], dp2[i])
-            # 트리플
-            if i >= j * 3:
-                if dp[i - (j * 3)] > dp[i] + 1:
-                    dp[i - (j * 3)] = dp[i] + 1            
-                    dp2[i - (j * 3)] = dp2[i] 
-                if dp[i - (j * 3)] == dp[i] + 1:        
-                    dp2[i - (j * 3)] = max(dp2[i - (j * 3)], dp2[i])
+                dp2[i - j] = max(dp2[i - j], dp2[i] + 1)  
+        # 더블, 트리플
+        for j in double_and_triple:
+            if j > i:
+                continue
+            if dp[i - j] > dp[i] + 1:
+                dp[i - j] = dp[i] + 1
+                dp2[i - j] = dp2[i]
+            if dp[i - j] == dp[i] + 1:
+                dp2[i - j] = max(dp2[i - j], dp2[i])   
     
     answer = [dp[0], dp2[0]]
     return answer
